7d6b35a5beefd4e342d0bd7e631947b2caea2a69
BrnFbrian <fbrian012345@gmail.com>
2023-09-16 16:37:35 -0300

    Tela de Loading

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,27 +2,32 @@ import React, { useEffect, useState } from 'react';
 import './Products.css';
 import fetchProducts from '../../api/fetchProducts';
 import ProductCard from '../ProductCard/ProductCard';
+import Loading from '../Loading/Loaging';
 
 
 function Products(){
 
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
 
     fetchProducts('iphone').then((response) => {
       setProducts(response);
+      setLoading(true);
       
     });
 
   }, []);
 
   return(
-    <section className="products container">
-      {
-        products.map((product) => <ProductCard key={product.id} data={product}/>)
-      }
-    </section>
+
+    ( loading && <Loading /> ) || (
+      <section className="products container">
+        {
+          products.map((product) => <ProductCard key={product.id} data={product}/>)
+        }
+      </section> )
   );
 }
 
